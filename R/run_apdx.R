5cb2160e9c499163afa0b8029d28d69558c909f9
Stos <astos1@awsacgnval002u.jnj.com>
2025-11-11 10:02:03 -0500

    Drop return_object function argument / factorised sections_structure / Other minor updates

--- R/run_apdx.R
+++ R/run_apdx.R
@@ -4,7 +4,7 @@
 #' @import uuid
 #' @import dplyr
 #' @import rlang
-NULL
+
 
 
 #' Create the appendix of a docx file - combine link creation and add TLF tables
@@ -14,10 +14,8 @@ NULL
 #' @param path_TLFs Path to the folder where your table outputs (TLFs) are saved.
 #' @param docx_object Path to the folder where your target docx is saved.
 #' @param keyword IMPORTANT: please ensure your doc has this unique keyword placed where you want the Appendix section to start.
-#' @param output FOR LATER: To select only some desired TLFs and not all.
-#' @param sections_structure: If set to Null then all outputs from path_TLFs will be used and no section will be created. Otherwise format of list, csv or excel and subsections.
-#' @param return_to_file Optional - if not specified, saving result docx file to the same folder with default name "RapTLR_Appendix_processed.docx". This function can auto-detect if the user specified a docx name along with it. If not, auto-create a file name and save to file.
-#' @param return_object Optional - if set to T, the function will return an officer object in global environment regardless of return_to_file
+#' @param sections_structure: Optionnal: If NULL then all outputs from path_TLFs will be used and no section will be created. Otherwise format of list, csv or excel and subsections.
+#' @param return_to_file Optionnal: Path and name of the docx document with the appendice. If NULL then the new docx document will be saved at the working directory as appendix_processed.docx 
 #' @returns NULL, output a docx to the destination folder.
 #' @examples
 #' path_TLFs = "REPLACE WITH YOUR OWN PATH C:/Documents/TLR_folder/TLF_outputs"
@@ -56,28 +54,46 @@ run_apdx <- function(path_TLFs,
                      docx_object,
                      keyword = "LISTOFAPPENDICES",
                      sections_structure = NULL,
-                     return_to_file = NULL,
-                     return_object = FALSE) {
+                     return_to_file = NULL) {
 
-  ##################################### Self Diagnostics #####################################
+  # Self Diagnostics ----
 
-  # Function argument that can't be missing or NULL
+  ## path_TLFs ----
   if (missing(path_TLFs) || is.null(path_TLFs)) {
     stop("Error: 'path_TLFs' cannot be NULL. Please specify a valid path.")
   }
+
+  if (!file.exists(path_TLFs)) stop("Error: Please provide a valid path for TLFs")
+  if (!any(str_detect(list.files(path_TLFs), "docx"))) stop("Error: No docx outputs found in 'path_TLFs'")
+  
+  ## docx_object ----
   if (missing(docx_object) || is.null(docx_object)) {
-    stop("Error: 'docx_object' cannot be NULL. Please specify a docx object.")
-  }
+    stop("Error: 'docx_object' cannot be NULL. Please specify a docx object.") }
+
+  if (!file.exists(docx_object)) stop("Error: Please provide a valid path for 'docx_object'")
+  if (!str_detect(docx_object, "docx")) stop("Error: No docx object in the path")
+
+  ## Keyword ----
   if (is.null(keyword)) {
     stop("Error: 'keyword' cannot be NULL. Please specify a keyword.")
   }
 
-  # If not NULL check it function argument exist
-  if (!file.exists(path_TLFs)) stop("Error: Please provide a valid path for TLFs")
-  if (!any(str_detect(list.files(path_TLFs), "docx"))) stop("Error: No docx outputs found in 'path_TLFs'")
+  ## return_to_file ----
+  if (is.null(return_to_file)) {
+    return_to_file <- file.path(getwd(), "appendix_processed.docx")
 
-  if (!file.exists(docx_object)) stop("Error: Please provide a valid path for 'docx_object'")
-  if (!str_detect(docx_object, "docx")) stop("Error: No docx object in the path")
+  } else {
+    return_to_file <- file.path(getwd(), return_to_file)
+
+    if (!dir.exists(dirname(return_to_file)))
+      stop("Error: Directory for 'return_to_file' does not exist")
+
+    if (!grepl("\\.docx$", return_to_file, ignore.case = TRUE))
+      return_to_file <- paste0(return_to_file, ".docx")
+  }
+
+  print( return_to_file )
+
 
   ##################################### Read in Sections Structure #####################################
   # Check output structure, if null all outputs otherwise outputs in Outputs columns
@@ -96,12 +112,16 @@ run_apdx <- function(path_TLFs,
                                     xlsx = readxl::read_excel(sections_structure, col_names = TRUE),
                                     stop("Unsupported file type!"))
 
+      if(ncol(output_structure_in) != 2) stop("Please import data with two columns only: Section and Outputs")
+      
       # Renaming Column to Section and Outputs
       output_structure_in <- output_structure_in %>%
-        rename(Section = 1,  Outputs = 2)
+        dplyr::rename(Section = 1,  Outputs = 2)
 
       # Transforming dataset into list
-      output_structure_list <- split(output_structure_in$Outputs, output_structure_in$Section)
+      output_structure_list <- split(output_structure_in$Outputs, 
+                                     factor(output_structure_in$Section, levels = unique(output_structure_in$Section)),
+                                     drop = FALSE)
 
       # If list return sections_structure
     } else if (is.list(sections_structure)) {
@@ -125,15 +145,10 @@ run_apdx <- function(path_TLFs,
   imported_docx_object <- officer::read_docx(docx_object)
 
 
-  #message("Outputs to be imported into the appendice: ", paste(str_remove(all_TLFs, ".docx"), collapse = ", "))
-
-
   ## This function is for catching addLink() function error
   ## If the return_to_file is not a valid path, catch the error and print to working directory instead
   addLink(path_TLFs = path_TLFs,
           outputs = all_TLFs)
-
-
 
 
   # Appendix creation
@@ -141,42 +156,9 @@ run_apdx <- function(path_TLFs,
                         path_TLFs = path_TLFs,
                         output_structure_list = output_structure_list,
                         keyword = "LISTOFAPPENDICES")
+  
 
-
-  ## This function is for the chunk right below.
-  ## If the return_to_file is not a valid path, catch the error and print to working directory instead
-  printToFile <- function(outTLR1, return_to_file) {
-    tryCatch(
-      {
-        message(paste0("Printing the result docx file with Appendix to ", return_to_file, "..."))
-        suppressWarnings(print(outTLR1, return_to_file))
-      },
-      error = function(cond) {
-        warning(paste0("return_to_file, the output path that you provided is invalid. Printing to the current working directory, which is \n",
-                       getwd(), "RapTLR_Appendix_processed.docx"))
-        print(outTLR1, "./RapTLR_Appendix_processed.docx")
-        NA
-      },
-      warning = function(cond) {
-        NULL
-      },
-      finally = {
-        NULL
-      }
-    )
-  }
-
-  ## If return_to_file is specified, print to file.
-  if (!is.null(return_to_file)) {
-    ## Use the function above. If the return_to_file is not a valid path,
-    ## catch the error and print to working directory instead
-    printToFile(outTLR1, return_to_file)
-  }
-
-  # Return the officer object if return_object is set to True, or if return_to_file is not specified
-  if (return_object == TRUE || is.null(return_to_file)) {
-    return(outTLR1)
-  }
-
+  print( outTLR1, return_to_file)
+  message(paste0("Printing the result docx file with Appendix to ", return_to_file))
 
 }
